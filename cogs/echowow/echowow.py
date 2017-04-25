fc5952926efbfde0fea61e415de11f7e9d128755
Zees3001 <info@outsideall.de>
2017-04-25 09:59:58 +0200

    Stupid spacing fixes...

--- cogs/echowow/echowow.py
+++ cogs/echowow/echowow.py
@@ -160,15 +160,13 @@ def get_artifact_info(player_dictionary):
 	relicItems = ["mainHand", "offHand"]
 	relicCount = 1
 
-        print "Init wowcheck"
-
-        for i in range(len(relicItems)):
+	for i in range(len(relicItems)):
             if (items[relicItems[i]]):
                 relicItem = items[relicItems[i]]
                 if (relicItem.quality === 6):
                     artifactRank = 0
                     relicCount += len(relicItems.relics)
-            
+                    
         if items.mainHand.artifactTraits[0]:
             for trait in range(len(items.mainHand.artifactTraits)):
                 artifactRank += items.mainHand.artifactTraits[trait].rank
